b3ceb0dc9276a0f6ce7c70e693192c5c51703f40
AlphaSlayer-001 <154256237+AlphaSlayer-001@users.noreply.github.com>
2024-03-29 15:14:09 +0530

    Update main.c
    -> Added braces in each case, which seemed to trigger an error on the mingw compiler

--- main.c
+++ main.c
@@ -18,7 +18,7 @@ int main(){
         scanf("%d", &choice);
 
         switch (choice) {
-            case 1:
+            case 1:{
                 char r_no[5];
                 int size = 0;
                 do{
@@ -28,20 +28,25 @@ int main(){
                 } while(size != 5);
                 print_grade_card(students, r_no, no_students);
                 break;
-            case 2:
+            }
+            case 2:{
                 add_new_student(students, &no_students);
                 break;
-            case 3:
+            }
+            case 3:{
                 //updateMarks(students, numStudents);
                 break;
-            case 4:
+            }
+            case 4:{
                 printf("\t\t\t\tExiting program...\n");
                 break;
-            default:
+            }
+            default:{
                 printf("\t\t\t\tInvalid choice! Please try again....\n\t\t\t\tPress any key to return..");
                 clear_input_buffer();
                 getchar();
                 clearScreen();
+            }
         }
     } while (choice != 4); // looping until the user chooses to exit
 
