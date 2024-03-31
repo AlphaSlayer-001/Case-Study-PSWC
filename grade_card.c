0747687f1c6944c70e23aac14910a430805cfe47
AlphaSlayer-001 <154256237+AlphaSlayer-001@users.noreply.github.com>
2024-03-31 12:02:50 +0530

    Update grade_card.c

--- grade_card.c
+++ grade_card.c
@@ -1,33 +1,34 @@
 #include "header.h"
 
-void print_grade_card(struct student *students, char *r_no, int no_students){
+void print_grade_card(int no_students, struct student *students, char *r_no) {
     clearScreen();
     display_design();
-    int i;
-    // to find the student with the specified roll number
-    for (i = 0; i < no_students; i++) {
+    int found = 0;
+    for (int i = 0; i < no_students; i++) {
+        //printf("%s\n", students[i].roll_no);
         if (strcmp(students[i].roll_no, r_no) == 0) {
-            break; // student found
+            // student found, print grade card
+            printf("\n\t\t\t\tName: %s\n", students[i].name);
+            printf("\t\t\t\tRoll No: %s\n", students[i].roll_no);
+            printf("\t\t\t\tMarks in ISA1: \n\t\t\t\t\tPSWC: %d \n\t\t\t\t\tPhysics: %d \n\t\t\t\t\tMathematics: %d\n", students[i].isa_1[0], students[i].isa_1[1], students[i].isa_1[2]);
+            printf("\t\t\t\tMarks in ISA2: \n\t\t\t\t\tPSWC: %d \n\t\t\t\t\tPhysics: %d \n\t\t\t\t\tMathematics: %d\n", students[i].isa_2[0], students[i].isa_2[1], students[i].isa_2[2]);
+            printf("\t\t\t\tMarks in ESA: \n\t\t\t\t\tPSWC: %d \n\t\t\t\t\tPhysics: %d \n\t\t\t\t\tMathematics: %d\n", students[i].esa[0], students[i].esa[1], students[i].esa[2]);
+            printf("\t\t\t\tTotal marks: \n\t\t\t\t\tPSWC: %.0f \n\t\t\t\t\tPhysics: %.0f \n\t\t\t\t\tMathematics: %.0f\n", students[i].total[0], students[i].total[1], students[i].total[2]);
+            printf("\t\t\t\tGrade in PSWC: %c\n", students[i].grade[0]);
+            printf("\t\t\t\tGrade in Physics: %c\n", students[i].grade[1]);
+            printf("\t\t\t\tGrade in Mathematics: %c\n", students[i].grade[2]);
+            found = 1;
+            break;
         }
     }
-  
-    // check if student with the specified roll number was found
-    if (i != no_students) {
-        printf("\t\t\t\t\t   Student with roll number %s not found.\n", r_no);
-        clear_input_buffer();
-        printf("\t\t\t\t\t   Press any key to return..\n");
-        getchar();
-        clearScreen();
-        return;
+
+    // if the student with the entered roll number is not found
+    if (!found) {
+        printf("\t\t\t\tStudent with roll number %s not found.\n", r_no);
     }
-    // print grade card for the found student
-    printf("\t\t\t\t\t\tGrade Card for Student with Roll Number: %s\n", r_no);
-    printf("\t\t\t\t\t\tName: %s\n", students[i].name);
-    printf("\t\t\t\t\t\tPSWC: %d\n\tGrade: %c\n", students[i].isa_1[0], students[i].grade[0]);
-    printf("\t\t\t\t\t\tPhysics: %d\n\tGrade: %c\n", students[i].isa_1[1], students[i].grade[1]);
-    printf("\t\t\t\t\t\tMathematics: %d\n\tGrade: %c\n", students[i].isa_1[2], students[i].grade[2]);
+
+    printf("\t\t\t\tPress any key to return..");
     clear_input_buffer();
-    printf("\t\t\t\t\t\tPress any key to return..\n");
-    getchar();
     clearScreen();
 }
+
