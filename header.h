a00bbe09c2c5123dbf42628ed5a957e71711e260
AlphaSlayer-001 <154256237+AlphaSlayer-001@users.noreply.github.com>
2024-03-31 12:03:15 +0530

    Update header.h

--- header.h
+++ header.h
@@ -1,9 +1,12 @@
 #include<stdio.h>
 #include<string.h>
+#include<ctype.h>
 #include<unistd.h>
 
 #define max_len 100
 #define max_no_students 100
+
+extern int no_students, temp;
 
 struct student{
     char name[max_len];
@@ -18,7 +21,9 @@ struct student{
 int str_size(char *string);
 void clear_input_buffer();
 void clearScreen();
+void whitespace(char *str);
 void details(struct student *students, int *no_students);
 void display_design();
-void print_grade_card(struct student *students, char *r_no, int no_students);
+void print_grade_card(int no_students, struct student *students, char *r_no);
 void add_new_student(struct student *students, int *no_students);
+void update_marks(struct student *students, int no_students);
